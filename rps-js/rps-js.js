b4a64d5d16746a75081b679c45e323e89c841984
K Amrutha Sai <128003134@sastra.ac.in>
2026-06-07 11:39:24 +0530

    intial commit, rps

---
+++ rps-js/rps-js.js
@@ -0,0 +1,86 @@
+  const score= JSON.parse(localStorage.getItem('score'))|| {
+    wins: 0,
+    losses: 0,
+    ties: 0
+  };
+
+  updateScoreElm();
+    
+  /*
+    if(!score){
+      score={
+        wins:0,
+        losses:0,
+        ties=0;
+      }
+    }
+    just another way
+  */
+
+  let cmpMov=' ';
+  function pickCmpMov(){
+    const randNum = Math.random();
+    if(randNum>=0&&randNum<1/3){
+      cmpMov='rock';
+    }else if(randNum>=1/3&&randNum<=2/3){
+      cmpMov='paper';
+    }else{ 
+      cmpMov='scissors';
+    }
+  }
+  function playGame(playerMov){
+  let res=' ';
+  
+  if(playerMov=='paper'){
+      if(cmpMov==='rock'){
+      res='you win';
+    }else if(cmpMov==='paper'){ 
+      res='tie';
+    }else{
+      res='you lose';
+    }
+  }
+
+  else if(playerMov=='scissors'){
+    if(cmpMov==='rock'){
+      res='you lose';
+    }else if(cmpMov==='paper'){ 
+      res='you win';
+    }else{
+      res='tie';
+    }
+  }
+
+  else {
+    if(cmpMov==='rock'){
+      res='tie';
+    }else if(cmpMov==='paper'){ 
+      res='you lose';
+    }else{
+      res='you win';
+    }  
+  }
+
+  if(res==='you win'){
+    score.wins+=1;
+  }else if(res==='you lose'){
+    score.losses+=1;
+  }else{
+    score.ties+=1;
+  }
+
+  localStorage.setItem('score', JSON.stringify(score));
+  updateScoreElm();
+
+  document.querySelector('.js-res')
+    .innerHTML= `${res}`;
+
+  document.querySelector('.js-mov')
+    .innerHTML=`your move:<img src="rpsemoj/${playerMov}-emoji.png" class="mov-icon"> - computer's move <img src="rpsemoj/${cmpMov}-emoji.png" class="mov-icon">`;
+
+}
+
+function updateScoreElm(){
+  document.querySelector('.js-score')
+  .innerHTML= `wins: ${score.wins}, losses: ${score.losses}, ties: ${score.ties} `;
+}
