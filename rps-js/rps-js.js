1828c1d6a41c9e2704e1ad78c6036a20ce474c99
K Amrutha Sai <128003134@sastra.ac.in>
2026-06-11 13:23:35 +0530

    added event listeners

--- rps-js/rps-js.js
+++ rps-js/rps-js.js
@@ -61,6 +61,17 @@
       }   
   }
 
+  document.body.addEventListener('keydown',(event)=>{
+    if(event.key==='r'){
+      playGame('rock');
+    }else if(event.key==='p'){
+      playGame('paper');
+    }
+    else if(event.key==='s'){
+      playGame('scissors');
+    }
+  });
+
   function playGame(playerMov){
   let res=' ';
   let cmpMov= pickCmpMov();
